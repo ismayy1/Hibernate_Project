8e5c9b243cdeb4e02297d982659460b1e8dfc34b
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-12-05 20:42:50 +0200

    commit

--- src/main/java/com/tpe/hb01/annotations/Student01.java
+++ src/main/java/com/tpe/hb01/annotations/Student01.java
@@ -1,20 +1,19 @@
 package com.tpe.hb01.annotations;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity // Classes marked with @Entity annotation correspond to table in the DB
 @Table(name = "t_student01")    // if we want to change the default table name, we use @Table annotation
 public class Student01 {    // Default table name = lowercase class name -> student01
 
     @Id     // primary key
-//    @Column(name = "std_id") // to change the name of the column
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "std_id") // to change the name of the column
     private int id;
 
-
+    @Column(name = "std_name", length = 60, nullable = false, unique = false)
     private String name;
 
+    @Transient  // column will not be created in the DB, the field is just to be used in the code and NOT in the DB
     private int grade;
 }
