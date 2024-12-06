6e6c384932e51d1d69a09a36fb648430b3571ad3
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-12-06 20:23:50 +0200

    commit

--- src/main/java/com/tpe/hb01/annotations/RunnerSave01.java
+++ src/main/java/com/tpe/hb01/annotations/RunnerSave01.java
@@ -13,6 +13,16 @@ public class RunnerSave01 {
         student01.setName("Bob Smith");
         student01.setGrade(90);
 
+        Student01 student02 = new Student01();
+        student02.setId(1002);
+        student02.setName("Mike Olsen");
+        student02.setGrade(88);
+
+        Student01 student03 = new Student01();
+        student03.setId(1003);
+        student03.setName("Molly Billa");
+        student03.setGrade(93);
+
 //        WE tell hibernate to get configurations from the specified file, and we tell it to read the Student01 class
 //        as annotated class
         Configuration configuration = new Configuration()
@@ -25,8 +35,9 @@ public class RunnerSave01 {
 
 //        Some operations, some lines of code
 
-        session.save(student01);    // INSERT INTO t_student01 (std_id, std_name, grade) VALUES (?, ?, ?)
-
+//        session.save(student01);    // INSERT INTO t_student01 (std_id, std_name, grade) VALUES (?, ?, ?)
+//        session.save(student02);
+        session.save(student03);
 
         transaction.commit();   // if it's not closed, the changes we made won't be saved to the DB
         session.close();
