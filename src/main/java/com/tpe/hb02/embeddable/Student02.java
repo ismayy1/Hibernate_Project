a608852d22335e05609792660c897c1309314ac1
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-12-07 20:24:46 +0200

    commit

--- src/main/java/com/tpe/hb02/embeddable/Student02.java
+++ src/main/java/com/tpe/hb02/embeddable/Student02.java
@@ -1,9 +1,6 @@
 package com.tpe.hb02.embeddable;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "t_student02")
@@ -16,4 +13,48 @@ public class Student02 {
     private String name;
 
     private int grade;
+
+    @Embedded   // It's Embedded from the Address Embeddable class. It's not required, but it's used for the code readability
+    private Address address;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Student02{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", grade=" + grade +
+                '}';
+    }
 }
