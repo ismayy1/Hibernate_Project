cbcea3e77900eea65bd8524a237f3af40db7a5dc
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-12-09 20:17:56 +0200

    commit

---
+++ src/main/java/com/tpe/hbo04/bi_onetoone/Dairy04.java
@@ -0,0 +1,13 @@
+package com.tpe.hbo04.bi_onetoone;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Dairy04 {
+
+    @Id
+    private int id;
+
+
+}
---
+++ src/main/java/com/tpe/hbo04/bi_onetoone/Student04.java
@@ -0,0 +1,61 @@
+package com.tpe.hbo04.bi_onetoone;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Student04 {
+
+    @Id
+    private int id;
+
+    private String name;
+
+    private int grade;
+
+    @OneToOne(mappedBy = "student")
+    private Dairy04 dairy;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public Dairy04 getDairy() {
+        return dairy;
+    }
+
+    public void setDairy(Dairy04 dairy) {
+        this.dairy = dairy;
+    }
+
+    @Override
+    public String toString() {
+        return "Student04{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", grade=" + grade +
+                ", dairy=" + dairy +
+                '}';
+    }
+}
