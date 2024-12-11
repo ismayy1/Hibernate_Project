6fd7cadaf4f185b7e5bc933a9d5adbb51a607d16
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-12-11 20:44:08 +0200

    commit

--- src/main/java/com/tpe/hbo08/manytomany/Student08.java
+++ src/main/java/com/tpe/hbo08/manytomany/Student08.java
@@ -23,4 +23,47 @@ public class Student08 {
     )
 //    If we customize our third table once, we don't need to customize it again anywhere
     private List<Book08> bookList = new ArrayList<>();
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getGreade() {
+        return greade;
+    }
+
+    public void setGreade(int greade) {
+        this.greade = greade;
+    }
+
+    public List<Book08> getBookList() {
+        return bookList;
+    }
+
+    public void setBookList(List<Book08> bookList) {
+        this.bookList = bookList;
+    }
+
+    @Override
+    public String toString() {
+        return "Student08{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", greade=" + greade +
+                ", bookList=" + bookList +
+                '}';
+    }
 }
