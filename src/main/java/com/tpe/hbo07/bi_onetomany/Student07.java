94acf8fb169396c8c988be5a08d3422d12d3a8c0
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-12-11 19:18:31 +0200

    commit

---
+++ src/main/java/com/tpe/hbo07/bi_onetomany/Student07.java
@@ -0,0 +1,65 @@
+package com.tpe.hbo07.bi_onetomany;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Student07 {
+
+    @Id
+    private int id;
+
+    @Column(name = "std_name", nullable = false)
+    private String name;
+
+    private int grade;
+
+    @OneToMany(mappedBy = "student")
+    private List<Book07> bookList = new ArrayList<>();
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public List<Book07> getBookList() {
+        return bookList;
+    }
+
+    public void setBookList(List<Book07> bookList) {
+        this.bookList = bookList;
+    }
+
+    @Override
+    public String toString() {
+        return "Student07{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", grade=" + grade +
+                ", bookList=" + bookList +
+                '}';
+    }
+}
