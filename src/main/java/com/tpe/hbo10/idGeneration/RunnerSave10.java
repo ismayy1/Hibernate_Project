f43a2cc3c15f0281fd26b87b5a81d31b4df01138
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-12-13 20:36:28 +0200

    commit

--- src/main/java/com/tpe/hbo10/idGeneration/RunnerSave10.java
+++ src/main/java/com/tpe/hbo10/idGeneration/RunnerSave10.java
@@ -1,2 +1,34 @@
-package com.tpe.hbo10.idGeneration;public class RunnerSave10 {
+package com.tpe.hbo10.idGeneration;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class RunnerSave10 {
+    public static void main(String[] args) {
+
+        Student10 student1 = new Student10();
+//        student1.setId(1001); // IDs are generated automatically
+        student1.setName("Bob");
+        student1.setGrade(99);
+
+        Student10 student2 = new Student10();
+//        student1000.setId(10001); // IDs are generated automatically
+        student2.setName("Mike");
+        student2.setGrade(68);
+
+
+        Configuration config = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student10.class);
+
+        SessionFactory sf = config.buildSessionFactory();
+        Session session = sf.openSession();
+        Transaction tx = session.beginTransaction();
+
+        tx.commit();
+        session.close();
+        sf.close();
+    }
 }
--- src/main/java/com/tpe/hbo10/idGeneration/Student10.java
+++ src/main/java/com/tpe/hbo10/idGeneration/Student10.java
@@ -1,2 +1,64 @@
-package com.tpe.hbo10.idGeneration;public class Student10 {
+package com.tpe.hbo10.idGeneration;
+
+import javax.persistence.*;
+
+@Entity
+public class Student10 {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+        /*
+            GenerationType.SEQUENCE ---> Used by OracleDB - PostgreSQL. Leaves the control to the developer/user. User/developer
+            can set some information like the starting value, or how many ids will be cached.
+
+            GenerationType.IDENTITY ---> Used by MySQL - MicrosoftSQL. The control is in Database hands. Database will create Ids
+            according to its own structure. The most simple, and most used Generation Type.
+
+            GenerationType.AUTO ---> Hibernate automatically finds the best strategy according to the database.
+
+            GenerationType.TABLE --> Slowest one. Because of this, it is the least used one. Creates an extra table to generate Ids.
+
+     */
+
+    @Column
+    private String name;
+
+    private int grade;
+
+    public int getId() {
+        return id;
+    }
+
+
+//    Generation type is IDENTITY, hence we're not allowed to set the id
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return "Student10{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", grade=" + grade +
+                '}';
+    }
 }
