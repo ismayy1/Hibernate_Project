2beeba86b26f6ef8c34d0d87cb70be151a827195
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-12-16 21:17:16 +0200

    commit

--- src/main/java/com/tpe/hbo12/get_load/RunnerSave12.java
+++ src/main/java/com/tpe/hbo12/get_load/RunnerSave12.java
@@ -1,8 +1,35 @@
 package com.tpe.hbo12.get_load;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 public class RunnerSave12 {
     public static void main(String[] args) {
 
+        Student12 student1 = new Student12();
+        student1.setName("Bob");
+        student1.setGrade(70);
 
+        Student12 student2 = new Student12();
+        student1.setName("Mike");
+        student1.setGrade(70);
+
+
+        Configuration configuration = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student12.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        session.save(student1);
+        session.save(student2);
+
+
+        transaction.commit();
+        session.close();
+        sessionFactory.close();
     }
 }
