c57b7aa2aba4a6519cf381f51dfe9f5f272f5fdf
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-12-17 19:43:01 +0200

    commit

---
+++ src/main/java/com/tpe/hbo014/entity_lifecycle2/Employee.java
@@ -0,0 +1,40 @@
+package com.tpe.hbo014.entity_lifecycle2;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "employee_id")
+    private Long id;
+
+    @Column(name = "employee_name")
+    private String name;
+
+    @Column(name = "employee_salary")
+    private Double salary;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+}
